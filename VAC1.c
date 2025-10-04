c5383268ea2904f36d70dd2c9184bf91e403eda8
shwet02052006 <shwet.9834@gmail.com>
2025-10-04 09:31:55 +0530

    .

---
+++ VAC1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+// default datatype *
+void main()
+{
+    printf("hello world! \n"); // string const
+    // primitive datatypes
+    printf("%d\n", 10);    // int const
+    printf("%f\n", 3.42f); // float const
+    printf("%c \n", 'A');  // char const
+
+    int a = 10;
+    printf("%d\n", a); // int const
+    float pi = 3.42;
+    printf("%f\n", pi); // float const
+    char ch = 'A';
+    printf("%c \n", ch); // char const
+    int  b=10; 
+    printf("%d\n",b);
+}
---
+++ VAC2.C
@@ -0,0 +1,90 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+//     int c;
+//     a = -20;
+//     b = -20;
+
+//     c = a / b;
+//     printf(" %d", c);
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+//     int c;
+//     a = 10;
+//     b = 20;
+//     int temp;
+//     printf("before swap : %d  \t %d \n", a, b);
+//     //with third var
+//     temp = a;
+//     a = b;
+//     b = temp;
+
+//     printf(" after swap : %d  \t %d \n", a, b);
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+  
+//     a = 10;
+//     b = 20;
+//     //without third var
+//     printf("before swap : %d  \t %d \n", a, b);
+//    a=a*b;
+//    b=a/b;
+//    a=a/b;
+
+
+//     printf(" after swap : %d  \t %d \n", a, b);
+//     return 0;
+// }
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b;
+  
+//     a = 1;
+//     b = 1;
+   
+  
+
+
+//     printf(" op  %d ", a||b);
+//     return 0;
+// }
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+  
+    a = 0;
+    a=!a;
+
+    printf(" op  %d ", a);
+    return 0;
+}
