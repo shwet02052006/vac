a53d2b3d7720a61c3ab3bf21194f542a83ee8875
Shwet Gajapure <shwet.9834@gmail.com>
2025-10-11 14:25:28 +0530

    .

--- pattern.c
+++ pattern.c
@@ -32,7 +32,7 @@ int main()
     //     printf("\n");
     // }
 
-    for (int row = 0; row < 5; row++)
+    for (int row = 0; row < 3; row++)
     {
         for (int col = 0; col <= row; col++)
         {
